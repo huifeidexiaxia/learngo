b7cccd03a3eef722923c4bbd56b3001323aece34
huifeidexiaxia <sywan2015@163.com>
2019-07-13 07:29:08 +0800

    add flag

--- src/day7/example/example1/balance/mgr.go
+++ src/day7/example/example1/balance/mgr.go
@@ -20,12 +20,13 @@ func RegisterBalancer(name string, b Balancer) {
 
 func DoBalance(name string, insts []*Instance) (inst *Instance, err error) {
 	balancer, ok := mgr.allBalancer[name]
-	if !ok {
+	if ok {
 		err = fmt.Errorf("Not found %s balancer", name)
 		return
 	}
 
 	fmt.Printf("use %s balancer\n", name)
-	inst, err = balancer.DoBalance(insts)
+	fmt.Printf("use %s balancer\n", balancer)
+	//inst, err = balancer.DoBalance(insts)
 	return
 }
